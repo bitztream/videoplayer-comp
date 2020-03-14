12ac3e72f5e6a82fb7af624838f8ebbf728ec80b
Alfred <alfred@kyles-mbp.gateway.sonic.net>
2020-03-14 02:05:02 -0700

    able to render video and a few tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
+/* eslint-disable import/extensions */
 import React from 'react';
+import axios from 'axios';
+import VideoPlayer from './Videoplayer.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      video: [],
     };
+    this.get = this.get.bind(this);
+  }
+
+  componentDidMount() {
+    this.get();
+  }
+
+  get() {
+    axios.get('/api/get')
+      .then((result) => {
+        this.setState({
+          video: result.data,
+        });
+      })
+      .catch((err) => console.log(err));
   }
 
   render() {
+    const { video } = this.state;
+    console.log(video);
     return (
-      <div>100</div>
+      <div><VideoPlayer video={video} /></div>
     );
   }
 }
