373b58e111252f4f809b4f9ff6fbe226ce18f09f
Alfred <alfred@kyles-mbp.gateway.sonic.net>
2020-03-14 16:45:36 -0700

    wrote a bit more tdd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,6 @@ class App extends React.Component {
 
   render() {
     const { video } = this.state;
-    console.log(video);
     return (
       <div><VideoPlayer video={video} /></div>
     );
