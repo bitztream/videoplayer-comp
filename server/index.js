88bd088f1a0ed7845b2792c9acd5637f810d7567
Alfred <alfred_guan@hotmail.com>
2020-05-31 21:48:09 -0700

    server fix

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const controller = require('./controller.js');
 
 const app = express();
 const port = 8080;
+const HOST = '0.0.0.0';
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,4 +14,4 @@ app.use('/', express.static(path.join(__dirname, '../client/dist')));
 app.get('/api/get', controller.avatar.get);
 
 // eslint-disable-next-line no-console
-app.listen(port, () => console.log(`listening on port ${port}!`));
+app.listen((port, HOST), () => console.log(`listening on http://${HOST}:${port}!`));
